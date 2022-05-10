243fffcda070b0a41cdb7e38e30964aae1c6ff4d
MarianaArzapalo <89662374+MarianaArzapalo@users.noreply.github.com>
2022-05-09 21:03:22 -0600

    Initial commit

---
+++ pyinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Autor: Ricardo Quintero
+# Description : Shell script para instalar librerias para Jupyter Notebook
+# Activación: chmod +x pylibinstall.sh
+
+pip install pandas
+pip install statsmodels
+pip install matplotlib
